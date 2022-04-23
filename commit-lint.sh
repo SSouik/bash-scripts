e002d0278991b10c6bb5076a51b7b46f3f442ddc
Sam Souik <me@samuelsouik.com>
2022-04-22 21:24:09 -0500

    updated commit lint script

--- commit-lint.sh
+++ commit-lint.sh
@@ -3,9 +3,30 @@
 REGEX="^(added|changed|fix|ci|docs|break): [A-za-z0-9]"
 COMMIT=""
 
+USAGE="
+Commit Lint CLI
+
+    --commit | -c     Commit message that should be linted
+    --regex  | -r     Custom regex to use to lint the commit message
+"
+
+VERSION="Commit Lint CLI - v1.0.0"
+if [[ $# -eq 0 ]]; then
+    echo "$USAGE"
+    exit
+fi
+
 while [[ $# -gt 0 ]]
 do
   case "$1" in
+    --help|-h)
+      echo "$USAGE";
+      exit
+      ;;
+    --version|-v)
+      echo "$VERSION"
+      exit
+      ;;
     --commit|-c)
       COMMIT=$2; shift 2
       ;;
@@ -13,9 +34,13 @@ do
       REGEX=$2; shift 2
       ;;
     *)
-      echo "Unsupported flag"
+      echo "Unsupported key $1"
+      echo "$USAGE"
       exit 1
   esac
+
+  [[ $# > 0 ]] && shift;
+
 done
 
 
