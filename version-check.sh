04039dfae4c874f49add49d443369adf63a7214c
Sam Souik <me@samuelsouik.com>
2022-04-25 21:17:30 -0500

    updated the version check script

--- version-check.sh
+++ version-check.sh
@@ -3,6 +3,10 @@
 USAGE="
 Version Check CLI
 
+Validate local version number against the upstream version.
+Only supports version numbers store in MAJOR.MINOR.PATCH
+and stored within a JSON file.
+
     --help    | -h     Show this help message
     --version | -v     Display the version of the CLI
     --file    | -f     Path to the version file (i.e package.json)
@@ -18,7 +22,7 @@ VERSION="Version Check CLI - v1.0.0"
 RAW_GITHUB_HOST="https://raw.githubusercontent.com"
 GITHUB_ORG=""
 GITHUB_REPO=""
-GITHUB_BRANCH=""
+GITHUB_BRANCH="main"
 PATH_TO_VERSION_FILE=""
 KEY_NAME="version"
 
@@ -71,5 +75,93 @@ UPSTREAM_VERSION=$(curl -s ${UPSTREAM_VERSION_FILE} | grep "\"${KEY_NAME}\"": | 
 # Get current version
 CURRENT_VERSION=$(grep "\"${KEY_NAME}\"": ${PATH_TO_VERSION_FILE} | cut -d\" -f4)
 
-echo "$UPSTREAM_VERSION"
-echo "$CURRENT_VERSION"
+echo
+echo
+echo "Upstream version: $UPSTREAM_VERSION"
+echo "Local version: $CURRENT_VERSION"
+echo
+
+upstream_versions=(${UPSTREAM_VERSION//./ })
+local_versions=(${CURRENT_VERSION//./ })
+
+# Upstream versions
+upstream_major=${upstream_versions[0]}
+upstream_minor=${upstream_versions[1]}
+upstream_patch=${upstream_versions[2]}
+
+# Local versions
+local_major=${local_versions[0]}
+local_minor=${local_versions[1]}
+local_patch=${local_versions[2]}
+
+# Major version is less than upstream
+if [[ $local_major -lt $upstream_major ]]; then
+  echo "Major version in local version is invalid."
+  exit 1
+fi
+
+# Major version was incremented too much i.e 1 -> 3
+if (( local_major > upstream_major + 1 )); then
+  update_to_version=$((upstream_major + 1))
+  echo "Major version must increment by 1: Update your local major version to ${update_to_version}"
+  exit 1
+fi
+
+# Major version is bumped, so minor and patch version should be 0
+if (( local_major == upstream_major + 1)); then
+  if (( local_minor != 0 )); then
+    echo "Minor version must be 0 when incrementing the major version."
+    exit 1
+  fi
+
+  if (( local_patch != 0 )); then
+    echo "Patch version must be 0 when incrementing the major version."
+    exit 1
+  fi
+
+  echo "Local version number is valid"
+  exit 0
+fi
+
+# Major version is not bumped
+# Minor version is less than upstream
+if [[ $local_minor -lt $upstream_minor ]]; then
+  echo "Minor version in local version is invalid."
+  exit 1
+fi
+
+# Minor version was incremented too much i.e 1 -> 3
+if (( local_minor > upstream_minor + 1 )); then
+  update_to_version=$((upstream_minor + 1))
+  echo "Minor version must increment by 1: Update your local minor version to ${update_to_version}"
+  exit 1
+fi
+
+# Minor version is bumped, so patch version must be 0
+if (( local_minor == upstream_minor + 1)); then
+  if (( local_patch != 0 )); then
+    echo "Patch version must be 0 when incrementing the minor version."
+    exit 1
+  fi
+
+  echo "Local version number is valid"
+  exit 0
+fi
+
+# Major and Minor versions are not bumped
+# Patch version is less than upstream
+if [[ $local_patch -lt $upstream_patch ]]; then
+  echo "Patch version in local version is invalid."
+  exit 1
+fi
+
+# Patch version is bumped by 1 and is valid
+if (( local_patch == upstream_patch + 1 )); then
+  echo "Local version number is valid"
+  exit 0
+fi
+
+update_to_version=$((upstream_patch + 1))
+echo "Patch version must increment by 1: Update your local patch version to ${update_to_version}"
+exit 1 
+
